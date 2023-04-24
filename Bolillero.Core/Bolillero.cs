a88cb9c05cec8fa1a9772d32305dd9ac8fd6a0d7
quirogapabon11 <quirogapabon11@gmail.com>
2023-04-24 17:10:35 -0300

    Avance 0.1 / 24-04-23
    Falta relaciones en el diagrama, agregar descripciones en la clase jugada y la interfaz jugada, faltan test y rellenar métodos.

---
+++ Bolillero.Core/Bolillero.cs
@@ -0,0 +1,35 @@
+﻿namespace Bolillero.Core;
+public class Bolillero
+{
+    public List<int>? Bolillas { get; set; }
+    public List<int>? Afuera { get; set; }
+    public List<int>? Jugada { get; set; }
+
+    public Bolillero(int bolillas, int afuera, int jugada)
+    {
+        this.Bolillas = new List<int>();
+        this.Afuera = new List<int>();
+        this.Jugada = new List<int>();
+    }
+
+    public bool Jugar(List<int> jugada)
+    {
+        for (i = 0; i <= jugada;)
+            return true;
+    }
+
+    public void SacarBolilla(int bolillas)
+    {
+
+    }
+
+    public void MeterBolilla(int afuera)
+    {
+
+    }
+
+    public void JugarNVeces()
+    {
+
+    }
+}
---
+++ Bolillero.Core/IAleatorio.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bolillero.Core
+{
+    public interface IAleatorio
+    {
+        
+    }
+}
